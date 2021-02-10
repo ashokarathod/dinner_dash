37560bf8dc60a581b3dc21d60ea242b30b5b935d
Ashok <exit>
2021-02-10 17:39:27 +0530

    removes column total and cost from the orders model..

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_09_171232) do
+ActiveRecord::Schema.define(version: 2021_02_10_120458) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -50,8 +50,6 @@ ActiveRecord::Schema.define(version: 2021_02_09_171232) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.integer "cost"
-    t.integer "total"
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
